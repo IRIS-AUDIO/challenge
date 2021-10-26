5c45d069151b88ac1647021d9e8d3c5657645f68
root <pjh_1996@naver.com>
2021-10-26 12:43:57 +0000

    add get_er function

--- metrics.py
+++ metrics.py
@@ -1,3 +1,35 @@
 import numpy as np
 import tensorflow as tf
+import json
 
+# label for metric (Class, Start, End)
+# predict(Class, Time)
+def get_er(predict, gt):
+    predict.sort(key = lambda x:x[1])
+    gt.sort(key = lambda x:x[1])
+    N = len(predict)
+    S = 0
+    D = 0
+    I = 0
+    reverse_I = len(predict)
+    for gt_item in gt:
+        temp_list = []
+        for pred_item in predict: # get all item between pred time
+            if (gt_item[1] <= pred_item[1]) and (pred_item[1] <= gt_item[2]):
+                temp_list.append(pred_item)
+                reverse_I -= 1
+        if len(temp_list) == 0:
+            D += 1
+        elif temp_list[0][0] != gt_item[0]: # check first item if wrong count S
+            S += 1
+            I += len(temp_list)-1 # else item are all 
+        else:
+            I += len(temp_list)-1 # else item are all 
+    I += reverse_I # case for wrong insertion 
+
+    return (S + D + I) / N
+
+if __name__ == '__main__':
+    gt = [[0, 10, 20],  [2, 45, 64], [1, 25, 32]]
+    predict = [[0,15], [1,16], [2, 22], [1, 34], [1,44], [2, 65]]
+    print(get_er(predict, gt))

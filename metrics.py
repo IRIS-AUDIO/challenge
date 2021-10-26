795627876e1954f2e36c611d63f4aea71718cffc
root <pjh_1996@naver.com>
2021-10-26 15:57:15 +0000

    modify to correct way

--- metrics.py
+++ metrics.py
@@ -8,28 +8,21 @@ import json
 def get_er(predict, gt):
     predict.sort(key = lambda x:x[1])
     gt.sort(key = lambda x:x[1])
-    N = len(predict)
-    S = 0
-    D = 0
-    I = 0
-    reverse_I = len(predict)
+    N = len(predict) + len(gt)
+    pred_N = len(predict)
+    answer = 0
     for gt_item in gt:
-        temp_list = []
-        for pred_item in predict: # get all item between pred time
+        for i, pred_item in enumerate(predict):
             if (gt_item[1] <= pred_item[1]) and (pred_item[1] <= gt_item[2]):
-                temp_list.append(pred_item)
-                reverse_I -= 1
-        if len(temp_list) == 0:
-            D += 1
-        elif temp_list[0][0] != gt_item[0]: # check first item if wrong count S
-            S += 1
-            I += len(temp_list)-1 # else item are all 
-        else:
-            I += len(temp_list)-1 # else item are all 
-    I += reverse_I # case for wrong insertion 
-    return (S + D + I) / N
+                if gt_item[0] == pred_item[0]:
+                    answer += 2 
+                    predict.remove(pred_item)
+                    break
+    return (N - answer) / pred_N
+
 
 if __name__ == '__main__':
-    gt = [[0, 10, 20],  [2, 45, 64], [1, 25, 32]]
-    predict = [[1,15], [1,16], [2, 22], [2,47], [1, 34], [1,44], [2, 65]]
-    print(get_er(predict, gt) == 1) # S=1, D =2, I=4, N=7
+    gt = [[0, 0, 10],  [2, 0, 20], [1, 15, 30], [2, 31, 40],  [1, 27, 32]]
+    predict = [[1,5], [1, 19], [2, 32], [2,38], [0, 38]]
+    print(get_er(predict, gt) == 1.2)
+    

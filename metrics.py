9053b09974acec5b3567ae00e62cf0e810956339
root <pjh_1996@naver.com>
2021-10-26 16:17:26 +0000

    add test code

--- metrics.py
+++ metrics.py
@@ -12,17 +12,15 @@ def get_er(predict, gt):
     pred_N = len(predict)
     answer = 0
     for gt_item in gt:
+        remove = False
         for i, pred_item in enumerate(predict):
             if (gt_item[1] <= pred_item[1]) and (pred_item[1] <= gt_item[2]):
                 if gt_item[0] == pred_item[0]:
                     answer += 2 
-                    predict.remove(pred_item)
+                    temp = pred_item
+                    remove = True
                     break
+        if remove:
+            predict.remove(temp)
     return (N - answer) / pred_N
-
-
-if __name__ == '__main__':
-    gt = [[0, 0, 10],  [2, 0, 20], [1, 15, 30], [2, 31, 40],  [1, 27, 32]]
-    predict = [[1,5], [1, 19], [2, 32], [2,38], [0, 38]]
-    print(get_er(predict, gt) == 1.2)
     

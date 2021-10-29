90b4f4e7b017d66a0aa5f75eef9f33557d05b62a
Seungjin Lee <lsg0385@naver.com>
2021-10-29 07:37:26 +0000

    [add] VAD based model

--- sj_train.py
+++ sj_train.py
@@ -1,5 +1,4 @@
 import argparse
-from markdown.extensions.attr_list import get_attrs
 import numpy as np
 import os
 import tensorflow as tf
@@ -16,52 +15,57 @@ from transforms import *
 from utils import *
 
 
-args = argparse.ArgumentParser()
-args.add_argument('--name', type=str, required=True)
-args.add_argument('--gpus', type=str, default='-1')
-args.add_argument('--model', type=str, default='EfficientNetB0')
-args.add_argument('--pretrain', type=bool, default=False)
-args.add_argument('--n_layers', type=int, default=0)
-args.add_argument('--n_dim', type=int, default=256)
-args.add_argument('--n_chan', type=int, default=1)
-args.add_argument('--n_classes', type=int, default=3)
+class ARGS:
+    def __init__(self) -> None:
+        self.args = argparse.ArgumentParser()
+        self.args.add_argument('--name', type=str, default='')
+        self.args.add_argument('--gpus', type=str, default='-1')
+        self.args.add_argument('--model', type=int, default=0)
+        self.args.add_argument('--v', type=int, default=2)
+        self.args.add_argument('--pretrain', type=bool, default=False)
+        self.args.add_argument('--n_layers', type=int, default=0)
+        self.args.add_argument('--n_dim', type=int, default=256)
+        self.args.add_argument('--n_chan', type=int, default=2)
+        self.args.add_argument('--n_classes', type=int, default=3)
 
-# DATA
-args.add_argument('--datapath', type=str, default='/root/datasets/Interspeech2020/generate_wavs/codes')
-args.add_argument('--background_sounds', type=str, default='drone_normed_complex_v3.pickle')
-args.add_argument('--voices', type=str, default='voice_normed_complex_v3.pickle')
-args.add_argument('--labels', type=str, default='voice_labels_mfc_v3.npy')
-args.add_argument('--noises', type=str, default='noises_specs_v2.pickle')
-args.add_argument('--test_background_sounds', type=str,
-                  default='test_drone_normed_complex_v2.pickle')
-args.add_argument('--test_voices', type=str, default='test_voice_normed_complex.pickle')
-args.add_argument('--test_labels', type=str, default='test_voice_labels_mfc.npy')
-args.add_argument('--n_mels', type=int, default=80)
+        # DATA
+        self.args.add_argument('--datapath', type=str, default='/root/datasets/Interspeech2020/generate_wavs/codes')
+        self.args.add_argument('--background_sounds', type=str, default='drone_normed_complex_v3.pickle')
+        self.args.add_argument('--voices', type=str, default='voice_normed_complex_v3.pickle')
+        self.args.add_argument('--labels', type=str, default='voice_labels_mfc_v3.npy')
+        self.args.add_argument('--noises', type=str, default='noises_specs_v2.pickle')
+        self.args.add_argument('--test_background_sounds', type=str,
+                        default='test_drone_normed_complex_v2.pickle')
+        self.args.add_argument('--test_voices', type=str, default='test_voice_normed_complex.pickle')
+        self.args.add_argument('--test_labels', type=str, default='test_voice_labels_mfc.npy')
+        self.args.add_argument('--n_mels', type=int, default=80)
 
-# TRAINING
-args.add_argument('--optimizer', type=str, default='adam',
-                  choices=['adam', 'sgd', 'rmsprop', 'adabelief'])
-args.add_argument('--lr', type=float, default=1e-3)
-args.add_argument('--end_lr', type=float, default=1e-3)
-args.add_argument('--lr_power', type=float, default=0.5)
-args.add_argument('--lr_div', type=float, default=2)
-args.add_argument('--clipvalue', type=float, default=0.01)
+        # TRAINING
+        self.args.add_argument('--optimizer', type=str, default='adam',
+                        choices=['adam', 'sgd', 'rmsprop', 'adabelief'])
+        self.args.add_argument('--lr', type=float, default=1e-3)
+        self.args.add_argument('--end_lr', type=float, default=1e-4)
+        self.args.add_argument('--lr_power', type=float, default=0.5)
+        self.args.add_argument('--lr_div', type=float, default=2)
+        self.args.add_argument('--clipvalue', type=float, default=0.01)
 
-args.add_argument('--epochs', type=int, default=500)
-args.add_argument('--batch_size', type=int, default=64)
-args.add_argument('--n_frame', type=int, default=2048)
-args.add_argument('--steps_per_epoch', type=int, default=100)
-args.add_argument('--l1', type=float, default=0)
-args.add_argument('--l2', type=float, default=1e-6)
-args.add_argument('--loss_alpha', type=float, default=0.8)
-args.add_argument('--loss_l2', type=float, default=1.)
-args.add_argument('--multiplier', type=float, default=10)
+        self.args.add_argument('--epochs', type=int, default=400)
+        self.args.add_argument('--batch_size', type=int, default=12)
+        self.args.add_argument('--n_frame', type=int, default=2048)
+        self.args.add_argument('--steps_per_epoch', type=int, default=100)
+        self.args.add_argument('--l1', type=float, default=0)
+        self.args.add_argument('--l2', type=float, default=1e-6)
+        self.args.add_argument('--loss_alpha', type=float, default=0.8)
+        self.args.add_argument('--loss_l2', type=float, default=1.)
+        self.args.add_argument('--multiplier', type=float, default=10)
 
-# AUGMENTATION
-args.add_argument('--snr', type=float, default=-15)
-args.add_argument('--max_voices', type=int, default=10)
-args.add_argument('--max_noises', type=int, default=6)
+        # AUGMENTATION
+        self.args.add_argument('--snr', type=float, default=-15)
+        self.args.add_argument('--max_voices', type=int, default=10)
+        self.args.add_argument('--max_noises', type=int, default=6)
 
+    def get(self):
+        return self.args.parse_args()
 
 def minmax_log_on_mel(mel, labels=None):
     # batch-wise pre-processing
@@ -86,14 +90,17 @@ def augment(specs, labels, time_axis=-2, freq_axis=-3):
     return specs, labels
 
 
-def to_density_labels(x, y):
+def to_frame_labels(x, y):
     """
     :param y: [..., n_voices, n_frames, n_classes]
     :return: [..., n_frames, n_classes]
     """
-    y = safe_div(y, tf.reduce_sum(y, axis=(-2, -1), keepdims=True))
     y = tf.reduce_sum(y, axis=-3)
     return x, y
+
+
+def mono_chan(x, y):
+    return x[..., :1], y
 
 
 def make_dataset(config, training=True, n_classes=3):
@@ -122,13 +129,15 @@ def make_dataset(config, training=True, n_classes=3):
                              n_classes=n_classes,
                              snr=config.snr,
                              min_ratio=1)
-    pipeline = pipeline.map(to_density_labels)
+    pipeline = pipeline.map(to_frame_labels)
     if training: 
         pipeline = pipeline.map(augment)
     pipeline = pipeline.batch(config.batch_size, drop_remainder=False)
     pipeline = pipeline.map(complex_to_magphase)
     pipeline = pipeline.map(magphase_to_mel(config.n_mels))
     pipeline = pipeline.map(minmax_log_on_mel)
+    if config.n_chan == 1:
+        pipeline = pipeline.map(mono_chan)
     return pipeline.prefetch(AUTOTUNE)
 
 
@@ -180,31 +189,9 @@ def custom_loss(alpha=0.8, l2=1):
     return _custom
 
 
-def f1_score(class_name):
-    f1 = tfa.metrics.F1Score(num_classes=2, threshold=0.5, average='micro')
-    def f1_man(y_true, y_pred):
-        # y_true (batch, frame)
-        y_true = tf.one_hot(tf.cast(y_true[...,0], tf.int32), 2)
-        y_pred = tf.one_hot(tf.cast(y_pred[...,0] > 0.5, tf.int32), 2)
-        return f1(y_true, y_pred)
-    
-    def f1_woman(y_true, y_pred):
-        # y_true (batch, frame)
-        y_true = tf.one_hot(tf.cast(y_true[...,1], tf.int32), 2)
-        y_pred = tf.one_hot(tf.cast(y_pred[...,1] > 0.5, tf.int32), 2)
-        return f1(y_true, y_pred)
-
-    def f1_kid(y_true, y_pred):
-        # y_true (batch, frame)
-        y_true = tf.one_hot(tf.cast(y_true[...,2], tf.int32), 2)
-        y_pred = tf.one_hot(tf.cast(y_pred[...,2] > 0.5, tf.int32), 2)
-        return f1(y_true, y_pred)
-    return get_attrs(f'f1_{class_name}')
-
-
 def cos_sim(y_true, y_pred):
     mask = tf.cast(
-        tf.reduce_sum(y_true, axis=-2) > 0., tf.float32) # [None, 30]
+        tf.reduce_sum(y_true, axis=-2) > 0., tf.float32) # [None, 3]
     mask = safe_div(mask, tf.reduce_sum(mask, axis=-1, keepdims=True))
     return tf.reduce_sum(
         tf.keras.losses.cosine_similarity(y_true, y_pred, axis=-2) * mask, 
@@ -223,19 +210,10 @@ def custom_scheduler(d_model, warmup_steps=4000, lr_div=2):
     return _scheduler
 
 
-if __name__ == "__main__":
-    config = args.parse_args()
-    os.environ['CUDA_VISIBLE_DEVICES'] = config.gpus
-    print(config)
-
-    TOTAL_EPOCH = config.epochs
-    BATCH_SIZE = config.batch_size
-    NAME = config.name if config.name.endswith('.h5') else config.name + '.h5'
-
-    """ MODEL """
+def get_model(config):
     input_tensor = tf.keras.layers.Input(
         shape=(config.n_mels, config.n_frame, config.n_chan))
-    backbone = getattr(tf.keras.applications.efficientnet, config.model)(
+    backbone = getattr(tf.keras.applications.efficientnet, f'EfficientNetB{config.model}')(
         include_top=False, weights=None, input_tensor=input_tensor)
 
     out = tf.transpose(backbone.output, perm=[0, 2, 1, 3])
@@ -247,14 +225,50 @@ if __name__ == "__main__":
         out = tf.keras.layers.Activation('sigmoid')(out) * out
 
     # v1 -------------------------
-    # out = tf.keras.layers.Bidirectional(tf.keras.layers.GRU(128, return_sequences=True))(out)
-    out = tf.keras.layers.Conv1D(input_tensor.shape[-2], 1, use_bias=False, data_format='channels_first')(out)
-    out = tf.keras.layers.Activation('relu')(out)
+    if config.v == 1:
+        out = tf.keras.layers.Conv1DTranspose(128, 2, 2)(out)
+        out = tf.keras.layers.Activation('relu')(out)
+        out = tf.keras.layers.Conv1DTranspose(64, 2, 2)(out)
+        out = tf.keras.layers.Activation('relu')(out)
+        out = tf.keras.layers.Conv1DTranspose(32, 2, 2)(out)
+        out = tf.keras.layers.Activation('relu')(out)
+        out = tf.keras.layers.Conv1DTranspose(16, 2, 2)(out)
+        out = tf.keras.layers.Activation('relu')(out)
+        out = tf.keras.layers.Conv1DTranspose(3, 2, 2)(out)
+        out = tf.keras.layers.Activation('relu')(out)
+
+    # v2 -------------------------
+    elif config.v == 2:
+        out = tf.keras.layers.Conv1DTranspose(128, 2, 2)(out)
+        out = tf.keras.layers.Conv1DTranspose(64, 2, 2)(out)
+        out = tf.keras.layers.Conv1DTranspose(32, 2, 2)(out)
+        out = tf.keras.layers.Conv1DTranspose(16, 2, 2)(out)
+        out = tf.keras.layers.Conv1DTranspose(3, 2, 2)(out)
+    else:
+        raise ValueError('wrong version')
+    
     out = tf.keras.layers.Dense(config.n_classes)(out)
     # out= tf.keras.layers.Activation('relu')(out)
     # out *= tf.cast(out < 1., out.dtype)
     out = tf.keras.layers.Activation('sigmoid')(out)
-    model = tf.keras.models.Model(inputs=input_tensor, outputs=out)
+    return tf.keras.models.Model(inputs=input_tensor, outputs=out)
+
+
+if __name__ == "__main__":
+    config = ARGS().get()
+    os.environ['CUDA_VISIBLE_DEVICES'] = config.gpus
+    print(config)
+
+    TOTAL_EPOCH = config.epochs
+    BATCH_SIZE = config.batch_size
+    NAME = (config.name + '_') if config.name != '' else ''
+    NAME = NAME + '_'.join([f'B{config.model}', f'v{config.v}', f'lr{config.lr}', 
+                            f'batch{config.batch_size}', f'opt_{config.optimizer}', 
+                            f'mel{config.n_mels}', f'chan{config.n_chan}'])
+    NAME = NAME if NAME.endswith('.h5') else NAME + '.h5'
+
+    """ MODEL """
+    model = get_model(config)
 
     lr = config.lr
     if config.optimizer == 'adam':
@@ -265,14 +279,15 @@ if __name__ == "__main__":
         opt = RMSprop(lr, momentum=0.9, clipvalue=config.clipvalue)
     else:
         opt = AdaBelief(lr, clipvalue=config.clipvalue)
-
     if config.l2 > 0:
         model = apply_kernel_regularizer(
             model, tf.keras.regularizers.l1_l2(config.l1, config.l2))
     model.compile(optimizer=opt, 
                 #   loss=custom_loss(alpha=config.loss_alpha, l2=config.loss_l2),
                   loss=tf.keras.losses.BinaryCrossentropy(),
-                  metrics=[cos_sim, f1_score('man'), f1_score('woman'), f1_score('kid')])
+                  metrics=[tf.keras.metrics.CosineSimilarity(name='tf_cos_sim', axis=-2),
+                           cos_sim,
+                           tfa.metrics.F1Score(num_classes=3, threshold=0.5, average='micro')])
     model.summary()
 
     if config.pretrain:
@@ -282,14 +297,16 @@ if __name__ == "__main__":
     """ DATA """
     train_set = make_dataset(config, training=True)
     test_set = make_dataset(config, training=False)
-    
+
     """ TRAINING """
     callbacks = [
-        CSVLogger(NAME.replace('.h5', '.log'), append=True),
+        CSVLogger(NAME.replace('.h5', '.csv'), append=True),
         SWA(start_epoch=TOTAL_EPOCH//2, swa_freq=2),
         ModelCheckpoint(NAME, monitor='val_loss', save_best_only=True,
                         verbose=1),
-        TerminateOnNaN()
+        TerminateOnNaN(),
+        TensorBoard(log_dir=os.path.join('tensorboard_log', NAME.split('.h5')[0])),
+        EarlyStopping(monitor='val_loss', patience=30, restore_best_weights=True)
     ]
 
     if not config.pretrain:
@@ -298,7 +315,7 @@ if __name__ == "__main__":
                 custom_scheduler(4096, TOTAL_EPOCH/12, config.lr_div)))
     else:
         callbacks.append(
-            ReduceLROnPlateau(monitor='loss', factor=0.9, patience=5))
+            ReduceLROnPlateau(monitor='val_loss', factor=1 / 2**0.5, patience=5, verbose=1))
 
     model.fit(train_set,
               epochs=TOTAL_EPOCH,

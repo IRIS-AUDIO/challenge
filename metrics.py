5b9a7237e74d9736fe07becb032c8893b83308f9
Seungjin Lee <lsg0385@naver.com>
2021-10-30 03:26:01 +0000

    [fix] bug fix for metrics.py

--- metrics.py
+++ metrics.py
@@ -29,15 +29,15 @@ def output_to_metric(cls0, cls1, cls2):
     answer_list = tf.cast(tf.zeros([0,2]), tf.int32)
 
     for item in cls0:
-        new_item = tf.expand_dims(tf.convert_to_tensor([tf.convert_to_tensor(0), (item[0] + item[1])/2]), axis=0)
+        new_item = tf.cast(tf.stack([0, (item[0] + item[1]) // 2], 0), item.dtype)[tf.newaxis, ...]
         answer_list = tf.concat([answer_list, new_item], axis=0)
 
     for item in cls1:
-        new_item = tf.expand_dims(tf.convert_to_tensor([tf.convert_to_tensor(1), (item[0] + item[1])/2]), axis=0)
+        new_item = tf.cast(tf.stack([1, (item[0] + item[1]) // 2], 0), item.dtype)[tf.newaxis, ...]
         answer_list = tf.concat([answer_list, new_item], axis=0)
 
     for item in cls2:
-        new_item = tf.expand_dims(tf.convert_to_tensor([tf.convert_to_tensor(2), (item[0] + item[1])/2]), axis=0)
+        new_item = tf.cast(tf.stack([2, (item[0] + item[1]) // 2], 0), item.dtype)[tf.newaxis, ...]
         answer_list = tf.concat([answer_list, new_item], axis=0)
 
     return answer_list
